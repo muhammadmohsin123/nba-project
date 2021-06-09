54dfff43c84e2ffaeb32adbac721774a74f28517
muhammadmohsin123 <mohsinsurani12@gmail.com>
2021-06-09 15:08:20 +0500

    working on nba site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Grid,
+  Container,
+  Typography,
+  Box,
+  Button,
+  AppBar,
+  Toolbar,
+  Paper,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box border={1} display="flex">
+      Hello world
+    </Box>
   );
 }
 
